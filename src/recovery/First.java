d28ca27ee09da40149a2a934be7f9588e20bde40
sandro <sjr1999@protonmail.com>
2022-04-15 14:02:27 -0300

    Initial commit

---
+++ src/recovery/First.java
@@ -0,0 +1,14 @@
+package recovery;
+
+import parser.*;
+
+public class First { //implementa os conjuntos first p/ alguns n.terminais
+
+    static public final RecoverySet main = new RecoverySet();
+    static public final RecoverySet identificador = new RecoverySet();
+    static public final RecoverySet declaracaoVariavel = new RecoverySet();
+
+    static {
+    	
+    }
+}
